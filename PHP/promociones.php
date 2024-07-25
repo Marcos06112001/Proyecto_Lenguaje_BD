6999a1a775149b6f972f118288b048dca827fe40
Cosmo0004 <nicolehidalgo437@gmail.com>
2024-07-25 17:32:00 -0600

    Bootstrap

--- PHP/promociones.php
+++ PHP/promociones.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Promociones de Productos Electrónicos</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/CSS/promo.css">
 </head>
 <body>
